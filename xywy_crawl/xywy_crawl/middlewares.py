1ce082236705848e7017213728237c3b99f6a21f
李亚国 <1075543143@qq.com>
2018-10-17 21:42:45 +0800

    scrapy 配置文件参数汇总

--- xywy_crawl/xywy_crawl/middlewares.py
+++ xywy_crawl/xywy_crawl/middlewares.py
@@ -56,57 +56,94 @@ from scrapy import signals
 #         spider.logger.info('Spider opened: %s' % spider.name)
 #
 
-class DownMiddleware1(object):
+# class DownMiddleware1(object):
+#
+#     def process_request(self, request, spider):
+#         """
+#         请求需要被下载时，经过所有下载器中间件的process_request调用
+#         :param request:
+#         :param spider:
+#         :return:
+#             None,继续后续中间件去下载；
+#             Response对象，停止process_request的执行，开始执行process_response
+#             Request对象，停止中间件的执行，将Request重新调度器
+#             raise IgnoreRequest异常，停止process_request的执行，开始执行process_exception
+#         """
+#         print(request.url)
+#         # print(123)
+#         # request.method = 'POST'
+#         return None
+#
+#     def process_response(self, request, response, spider):
+#         """
+#         spider处理完成，返回时调用
+#         :param response:
+#         :param result:
+#         :param spider:
+#         :return:
+#             Response 对象：转交给其他中间件process_response
+#             Request 对象：停止中间件，request会被重新调度下载
+#             raise IgnoreRequest 异常：调用Request.errback
+#         """
+#         # print('response1')
+#         return response
+#
+#     def process_exception(self, request, exception, spider):
+#         """
+#         当下载处理器(download handler)或 process_request() (下载中间件)抛出异常
+#         :param response:
+#         :param exception:
+#         :param spider:
+#         :return:
+#             None：继续交给后续中间件处理异常；
+#             Response对象：停止后续process_exception方法
+#             Request对象：停止中间件，request将会被重新调用下载
+#         """
+#         print(122)
+#         return None
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         # print(222)
+#         # crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
 
+from scrapy.contrib.downloadermiddleware.httpproxy import HttpProxyMiddleware
+import base64
+import six
+import random
+
+
+def to_bytes(text, encoding=None, errors='strict'):
+    if isinstance(text, bytes):
+        return text
+    if not isinstance(text, six.string_types):
+        raise TypeError('to_bytes must receive a unicode, str or bytes '
+                        'object, got %s' % type(text).__name__)
+    if encoding is None:
+        encoding = 'utf-8'
+    return text.encode(encoding, errors)
+
+class ProxyMiddleware(object):
     def process_request(self, request, spider):
-        """
-        请求需要被下载时，经过所有下载器中间件的process_request调用
-        :param request:
-        :param spider:
-        :return:
-            None,继续后续中间件去下载；
-            Response对象，停止process_request的执行，开始执行process_response
-            Request对象，停止中间件的执行，将Request重新调度器
-            raise IgnoreRequest异常，停止process_request的执行，开始执行process_exception
-        """
-        print(request.url)
-        print(123)
-        # request.method = 'POST'
-        return None
-
-    def process_response(self, request, response, spider):
-        """
-        spider处理完成，返回时调用
-        :param response:
-        :param result:
-        :param spider:
-        :return:
-            Response 对象：转交给其他中间件process_response
-            Request 对象：停止中间件，request会被重新调度下载
-            raise IgnoreRequest 异常：调用Request.errback
-        """
-        print('response1')
-        return response
-
-    def process_exception(self, request, exception, spider):
-        """
-        当下载处理器(download handler)或 process_request() (下载中间件)抛出异常
-        :param response:
-        :param exception:
-        :param spider:
-        :return:
-            None：继续交给后续中间件处理异常；
-            Response对象：停止后续process_exception方法
-            Request对象：停止中间件，request将会被重新调用下载
-        """
-        print(122)
-        return None
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        # print(222)
-        # crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
+        PROXIES = [
+            {'ip_port': '14.115.107.72:9797', 'user_pass': ''},
+            # {'ip_port': '120.198.243.22:80', 'user_pass': ''},
+            # {'ip_port': '111.8.60.9:8123', 'user_pass': ''},
+            # {'ip_port': '101.71.27.120:80', 'user_pass': ''},
+            # {'ip_port': '122.96.59.104:80', 'user_pass': ''},
+            # {'ip_port': '122.224.249.122:8088', 'user_pass': ''},
+        ]
+        proxy = random.choice(PROXIES)
+        print(proxy)
+        if proxy['user_pass'] is not None:
+            request.meta['proxy'] = to_bytes("http://%s" % proxy['ip_port'])
+            # encoded_user_pass = base64.encodestring(to_bytes(proxy['user_pass']))
+            encoded_user_pass = base64.encodebytes(to_bytes(proxy['user_pass']))
+            request.headers['Proxy-Authorization'] = to_bytes('Basic ' + encoded_user_pass)
+            print("**************ProxyMiddleware have pass************" + proxy['ip_port'])
+        else:
+            print("**************ProxyMiddleware no pass************" + proxy['ip_port'])
+            request.meta['proxy'] = to_bytes("http://%s" % proxy['ip_port'])
--- xywy_crawl/xywy_crawl/settings.py
+++ xywy_crawl/xywy_crawl/settings.py
@@ -9,59 +9,105 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
+# 1. 爬虫名称
 BOT_NAME = 'xywy_crawl'
-
+# 2. 爬虫应用路径
 SPIDER_MODULES = ['xywy_crawl.spiders']
 NEWSPIDER_MODULE = 'xywy_crawl.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
+# 3. 客户端 user-agent请求头
 #USER_AGENT = 'xywy_crawl (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
+# 5. 并发请求数
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
+# 6. 延迟下载秒数
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
+#单域名访问并发数，并且延迟下次秒数也应用在每个域名
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# 单IP访问并发数，如果有值则忽略：CONCURRENT_REQUESTS_PER_DOMAIN，并且延迟下次秒数也应用在每个IP
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
+# 8. 是否支持cookie，cookiejar进行操作cookie
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
+# 9. Telnet用于查看当前爬虫的信息，操作爬虫等...
+#    使用telnet ip port ，然后通过命令操作
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
+# 10. 默认请求头
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
+#11 爬虫中间件
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-   # 'xywy_crawl.middlewares.XywyCrawlSpiderMiddleware': 543,
-   'xywy_crawl.middlewares.DownMiddleware1': 300,
-}
+# SPIDER_MIDDLEWARES = {
+#    # 'xywy_crawl.middlewares.XywyCrawlSpiderMiddleware': 543,
+#    'xywy_crawl.middlewares.DownMiddleware1': 300,
+# }
 
 # Enable or disable downloader middlewares
+#12下载中间件
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'xywy_crawl.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+# DOWNLOADER_MIDDLEWARES = {
+#    'xywy_crawl.middlewares.DownMiddleware1': 543,
+# }
 
-# Enable or disable extensions 自定义扩展，基于信号进行调用
+
+#13自定义扩展，基于信号进行调用
+# Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
 # EXTENSIONS = {
 #    'xywy_crawl.extensions.MyExtension': 300,
 # }
+
+
+
+# 13. 爬虫允许的最大深度，可以通过meta查看当前深度；0表示无深度
+# DEPTH_LIMIT = 3
+
+# 14. 爬取时，0表示深度优先Lifo(默认)；1表示广度优先FiFo
+
+# 后进先出，深度优先
+# DEPTH_PRIORITY = 0
+# SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleLifoDiskQueue'
+# SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.LifoMemoryQueue'
+# 先进先出，广度优先
+
+# DEPTH_PRIORITY = 1
+# SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
+# SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
+
+# 15. 调度器队列
+# SCHEDULER = 'scrapy.core.scheduler.Scheduler'
+# from scrapy.core.scheduler import Scheduler
+
+# DUPEFILTER_CLASS = 'scrapy.dupefilters.RFPDupeFilter'
+
+DUPEFILTER_CLASS = 'xywy_crawl.dupefilters.WwwDupeFilter'  #自定义去重规则
+
+
+
+
+
+
+
 
 # Configure item pipelines  定义pipeline处理请求
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
@@ -70,6 +116,7 @@ SPIDER_MIDDLEWARES = {
 # }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
+# 17. 自动限速算法
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
 # The initial download delay
@@ -90,7 +137,57 @@ SPIDER_MIDDLEWARES = {
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
+# 18. 自定制命令
+COMMANDS_MODULE ="xywy_crawl.commands"
 
+"""
+19. 代理，需要在环境变量中设置
+    from scrapy.contrib.downloadermiddleware.httpproxy import HttpProxyMiddleware
 
+    方式一：使用默认
+        os.environ
+        {
+            http_proxy:http://root:woshiniba@192.168.11.11:9999/
+            https_proxy:http://192.168.11.11:9999/
+        }
+    方式二：使用自定义下载中间件
 
-# DUPEFILTER_CLASS = 'xywy_crawl.rep.RepeatUrl'  #自定义去重规则
+    def to_bytes(text, encoding=None, errors='strict'):
+        if isinstance(text, bytes):
+            return text
+        if not isinstance(text, six.string_types):
+            raise TypeError('to_bytes must receive a unicode, str or bytes '
+                            'object, got %s' % type(text).__name__)
+        if encoding is None:
+            encoding = 'utf-8'
+        return text.encode(encoding, errors)
+
+    class ProxyMiddleware(object):
+        def process_request(self, request, spider):
+            PROXIES = [
+                {'ip_port': '111.11.228.75:80', 'user_pass': ''},
+                {'ip_port': '120.198.243.22:80', 'user_pass': ''},
+                {'ip_port': '111.8.60.9:8123', 'user_pass': ''},
+                {'ip_port': '101.71.27.120:80', 'user_pass': ''},
+                {'ip_port': '122.96.59.104:80', 'user_pass': ''},
+                {'ip_port': '122.224.249.122:8088', 'user_pass': ''},
+            ]
+            proxy = random.choice(PROXIES)
+            if proxy['user_pass'] is not None:
+                request.meta['proxy'] = to_bytes（"http://%s" % proxy['ip_port']）
+                encoded_user_pass = base64.encodestring(to_bytes(proxy['user_pass']))
+                request.headers['Proxy-Authorization'] = to_bytes('Basic ' + encoded_user_pass)
+                print "**************ProxyMiddleware have pass************" + proxy['ip_port']
+            else:
+                print "**************ProxyMiddleware no pass************" + proxy['ip_port']
+                request.meta['proxy'] = to_bytes("http://%s" % proxy['ip_port'])
+
+    DOWNLOADER_MIDDLEWARES = {
+       'step8_king.middlewares.ProxyMiddleware': 500,
+    }
+
+"""
+
+DOWNLOADER_MIDDLEWARES = {
+   'xywy_crawl.middlewares.ProxyMiddleware': 500,
+}
